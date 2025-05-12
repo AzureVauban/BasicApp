de8623c11acdf51405014030822e54a054bfa45b
Michael Elder <niv14@txstate.edu>
2025-05-11 23:16:16 -0500

    added 3 basic placeholder buttons
    added formatting

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,5 @@
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import React, { useEffect } from "react";
-import { Text, View } from "react-native";
-
-// Define the navigation prop type
-type RootStackParamList = {
-  home: undefined;
-};
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  // Cast navigation as a StackNavigationProp
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-
-  // Automatically navigate to "home"
-  useEffect(() => {
-    navigation.replace("home");
-  }, [navigation]);
-
-  return (
-    <View>
-      <Text>Redirecting to Home...</Text>
-    </View>
-  );
+  return <Redirect href="/home" />;
 }
